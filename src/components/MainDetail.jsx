22566c5e9bcaed835c3e023888462ae54aab62b4
Jonathon Wilks <jonathondwilks@icloud.com>
2021-06-15 19:18:36 +0100

    first commit

---
+++ src/components/MainDetail.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { getCriptoUpdateUrl } from "../constants";
+
+// This function give us the current time in seconds
+function currentTime() {
+  return Math.round(Date.now() / 1000);
+}
+
+/*
+  Use this function with the updated_at timestamp you get from each coin item in the API response
+ */
+function convertToSeconds(dateValue) {
+  // This guard is needed due to the API discrepancies in handling dates
+  return typeof dateValue === "string"
+    ? Math.round(Date.parse(dateValue) / 1000)
+    : dateValue;
+}
+
+export default function MainDetail({ selectedCripto }) {
+  const [currentLiveCryptoInfo, setcurrentLiveCryptoInfo] = useState({});
+
+  useEffect(
+    function () {
+      console.log("use effect is running within main");
+      fetch(getCriptoUpdateUrl(selectedCripto))
+        .then((response) => response.json())
+        .then(function (liveCryptoFromServer) {
+          setcurrentLiveCryptoInfo(liveCryptoFromServer[selectedCripto]);
+          console.log("this is our state after fetch", currentLiveCryptoInfo);
+        });
+    },
+    [selectedCripto]
+  );
+
+  return (
+    <>
+      <section className="main-detail__central">
+        <div className="main-detail__update">
+          {/* This part is for the challenge */}
+        </div>
+        <div className="main-detail__name">{selectedCripto}</div>
+        <div className="main-detail__price">
+          {`£${currentLiveCryptoInfo.gbp}`}
+        </div>
+      </section>
+    </>
+  );
+}
+
+// fetch to this getCriptoUpdateUrl to generate the real time price
---
+++ src/components/SideListItem.jsx
@@ -0,0 +1,19 @@
+export default function SideListItem({
+  isSelectedCripto,
+  setSelectedCripto,
+  cryptoCurrencies,
+  // item: { id, name }
+}) {
+  return cryptoCurrencies.map(function (currencies) {
+    return (
+      <li key={currencies.id}>
+        <button
+          className={isSelectedCripto(currencies.id) ? "selected" : ""}
+          onClick={() => setSelectedCripto(currencies.id)}
+        >
+          {currencies.name}
+        </button>
+      </li>
+    );
+  });
+}
